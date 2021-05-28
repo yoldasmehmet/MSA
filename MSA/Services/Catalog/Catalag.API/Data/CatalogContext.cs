c22b3444d949ecfdbc86152011ed0ab5754fa0fd
Mehmet YOLDAŞ <mehmet.yoldas@gazi.edu.tr>
2021-05-28 19:59:58 +0300

    .

---
+++ MSA/Services/Catalog/Catalag.API/Data/CatalogContext.cs
@@ -0,0 +1,26 @@
+﻿using Catalag.API.Data.Interfaces;
+using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalag.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var dbsettings = configuration.GetSection("DtabaseSettings");
+            var connstr = dbsettings.GetValue<string>("ConnectionString");
+            var dbName = dbsettings.GetValue<string>("DatabaseName");
+            var collectionName = dbsettings.GetValue<string>("CollectionName");
+            var client = new MongoDB.Driver.MongoClient(connstr);
+            var database = client.GetDatabase(dbName);
+            Products = database.GetCollection<Product>(collectionName);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
---
+++ MSA/Services/Catalog/Catalag.API/Data/Interfaces/ICatalogContext.cs
@@ -0,0 +1,15 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalag.API.Data.Interfaces
+
+{
+    public class ICatalogContext
+    {
+        IMongoCollection<Product> Products { get; }
+    }
+}
